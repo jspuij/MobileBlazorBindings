c531bbb8a0009c4fbd99c2a8fb1f2f719bd67fa8
Jan-Willem Spuij <jws@birdseyeview.nl>
2020-11-10 13:37:29 +0100

    Base Auth structure added.

---
+++ samples/AuthSample/AuthApp/App.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.MobileBlazorBindings;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AuthApp
+{
+    public class App : Application
+    {
+        public App(string[] args = null, IFileProvider fileProvider = null)
+        {
+            var hostBuilder = MobileBlazorBindingsHost.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Adds web-specific services such as NavigationManager
+                    services.AddBlazorHybrid();
+
+                    // Register app-specific services
+                    services.AddSingleton<CounterState>();
+                    services.AddOidcAuthentication(options =>
+                    {
+                        options.ProviderOptions.Authority = "https://demo.identityserver.io/";
+                        options.ProviderOptions.ClientId = "interactive.public";
+                        options.ProviderOptions.DefaultScopes.Add("openid");
+                        options.ProviderOptions.DefaultScopes.Add("profile");
+                        options.ProviderOptions.PostLogoutRedirectUri = "/";
+                        options.ProviderOptions.ResponseType = "code";
+                    });
+                })
+                .UseWebRoot("wwwroot");
+
+            if (fileProvider != null)
+            {
+                hostBuilder.UseStaticFiles(fileProvider);
+            }
+            else
+            {
+                hostBuilder.UseStaticFiles();
+            }
+            var host = hostBuilder.Build();
+
+            MainPage = new ContentPage { Title = "My Application" };
+            host.AddComponent<Main>(parent: MainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/Microsoft.MobileBlazorBindings.Authentication/Services/OidcAuthenticationService.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+
+namespace Microsoft.MobileBlazorBindings.Authentication.Services
+{
+    /// <summary>
+    /// The default implementation for <see cref="IRemoteAuthenticationService{TRemoteAuthenticationState}"/> that uses JS interop to authenticate the user.
+    /// </summary>
+    /// <typeparam name="TRemoteAuthenticationState">The state to preserve across authentication operations.</typeparam>
+    /// <typeparam name="TAccount">The type of the <see cref="RemoteUserAccount" />.</typeparam>
+    /// <typeparam name="TProviderOptions">The options to be passed down to the underlying JavaScript library handling the authentication operations.</typeparam>
+    public class RemoteAuthenticationService<TRemoteAuthenticationState, TAccount, TProviderOptions> :
+        AuthenticationStateProvider,
+        IRemoteAuthenticationService<TRemoteAuthenticationState>,
+        IAccessTokenProvider
+        where TRemoteAuthenticationState : RemoteAuthenticationState
+        where TProviderOptions : new()
+        where TAccount : RemoteUserAccount
+    {
+        public Task<RemoteAuthenticationResult<TRemoteAuthenticationState>> CompleteSignInAsync(RemoteAuthenticationContext<TRemoteAuthenticationState> context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RemoteAuthenticationResult<TRemoteAuthenticationState>> CompleteSignOutAsync(RemoteAuthenticationContext<TRemoteAuthenticationState> context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ValueTask<AccessTokenResult> RequestAccessToken()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ValueTask<AccessTokenResult> RequestAccessToken(AccessTokenRequestOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RemoteAuthenticationResult<TRemoteAuthenticationState>> SignInAsync(RemoteAuthenticationContext<TRemoteAuthenticationState> context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RemoteAuthenticationResult<TRemoteAuthenticationState>> SignOutAsync(RemoteAuthenticationContext<TRemoteAuthenticationState> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
